cd293d902ce16f5ca981038dedc0ad897b7babd3
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-05-23 20:53:51 -0500

    got it to show 100

--- api/main.py
+++ api/main.py
@@ -1,12 +1,43 @@
-import psycopg
+
 from fastapi import FastAPI
+import psycopg
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# GET path /api/categories
+# id, title, canon
+# categories
+# /api/categories?<int:page>
+# example:   /api/categories?page=17
+conn_str = "postgresql://trivia-game:trivia-game@db/trivia-game"
+
+@app.get("/api/categories")
+def list_categories(page: int=0):
+	#                        username   :password   @ database"
+	with psycopg.connect(conn_str) as conn:
+		with conn.cursor() as cur:
+			cur.execute("""
+				SELECT id, title, canon
+				FROM categories
+				LIMIT 100 OFFSET %s
+			""", [page * 100])
+			records = cur.fetchall()
+			results = []
+			#to make it a pretty dictionary
+			for record in records:
+				# record = [1, ""!"", true]
+				results.append({
+					"id": record[0],
+					"title": record[1],
+					"canon": record[2],
+				})
+			return results
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
 
 
 # @app.get("/api/categories")
